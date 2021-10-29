4dc8b3517c50941365467e660d7a3a4123ebaef5
Tejas102 <39779022+Tejas102@users.noreply.github.com>
2021-10-30 01:44:03 +0530

    api deployed
    api deployed. api shown in html. added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,30 @@ import Header from './Header.jsx';
 
 const App = () => {
   return (
+    
     <div className='container'>
       <Header/>
-      <div className="container-view">Some activities should be here</div>
-      <div>testing changes</div>
-      <div>redeployed</div>
+      <div className="container-view"></div>
+      <div className="container-view">
+        <button>Archive all calls</button>
+      <div className="containter-view" id="root"></div>  
+
+
+      </div>
     </div>
   );
 };
+
+fetch('https://aircall-job.herokuapp.com/activities/7834').then((data)=>{
+  // console.log(data);
+  return data.json();
+}).then((completedata)=>{
+  console.log(completedata.to);
+  document.getElementById('root').innerHTML=completedata.to;
+}).catch((err)=>{
+  console.log(err);
+})
+
 
 ReactDOM.render(<App/>, document.getElementById('app'));
 
